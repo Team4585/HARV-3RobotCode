b24cdbd8bf601d7526ee2ead3d3681726f381912
Silver <silvertorch5@gmail.com>
2014-10-11 00:47:04 -0600

    RobotCode Added
    Basic Robot Code Structure Added
    More Javadoc Added

---
+++ src/frc/team4585/robot/RobotCode.java
@@ -0,0 +1,36 @@
+
+package frc.team4585.robot;
+
+import edu.wpi.first.wpilibj.SimpleRobot;
+
+/**
+ * Contains All Autonomous and Operator Methods and Code.
+ * 
+ * @author AutonomousDevs, OperatorDevs
+ * @version 0.0.1 
+ * @since 10/10/2014
+ */
+
+public class RobotCode extends SimpleRobot
+{    
+    
+    /**
+     * This function is called once each time the robot enters Autonomous control.
+     */
+     public void autonomousCode()
+     {
+         
+         //AutonomousDevs Code
+         
+     }
+     
+    /**
+     * This function is called once each time the robot enters Operator control.
+     */
+    public void operatorCode()
+    {
+      
+        //OperatorDevs Code
+    }    
+    
+}
